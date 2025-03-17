2d3abfa2e98e378d53a9845dcfc7d9c160e28dc3
Himanshu Ranjan <himanshu.ranjan@razorpay.com>
2025-03-17 11:28:50 +0530

    keep consumer up

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"log"
 
 	"github.com/IBM/sarama"
 	"gocloud.dev/pubsub/kafkapubsub"
+
+	"github.com/himanshu-redd/kafka-hands-on/entities"
 )
 
 func main() {
@@ -14,7 +16,7 @@ func main() {
 	saramaConfig := sarama.NewConfig()
 	saramaConfig.Producer.Timeout = 5
 	saramaConfig.Producer.Transaction.Retry.Max = 20
-	saramaConfig.Producer.Return.Successes = true
+	saramaConfig.Producer.Return.Successes = false
 
 	topic, err := kafkapubsub.OpenSubscription([]string{"localhost:9092"},
 		saramaConfig,
@@ -25,11 +27,21 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	msg, err := topic.Receive(context.Background())
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	for {
+		msg, err := topic.Receive(context.Background())
 
-	fmt.Println("Message received: ", string(msg.Body))
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		if msg != nil {
+			var weather entities.WeatherResponse
+			err := json.Unmarshal(msg.Body, &weather)
+			if err != nil {
+				log.Fatalf("json ummarshal failed : %v\n", err.Error())
+			}
+			log.Printf("Current weather: %v\n", weather.Weather[0].Description)
+		}
+	}
 
 }
